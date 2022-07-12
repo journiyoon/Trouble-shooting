8b5b0d7129078bd254d9cbd8ef9b995417eaedf4
journiyoon <journiyoon@gmail.com>
2022-07-12 16:02:25 +0900

    webstorm setting tabwidth 2

--- indenttest/webstorm.js
+++ indenttest/webstorm.js
@@ -1,10 +1,8 @@
+// write webstorm
 for (let i = 0; i < 10; i++) {
   console.log(i);
   let array = [1, 2, 3];
 }
 for (let i = 0; i < 9; i++) {
-  console.log(i);
-}
-for (let i = 0; i < 100; i++) {
   console.log(i);
 }
