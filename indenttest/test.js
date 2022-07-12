2658d87eda8a9f1c6ef5d751e5a49b2e5ed6e43f
journiyoon <journiyoon@gmail.com>
2022-07-12 16:01:34 +0900

    prettier setting tabwidth 1

--- indenttest/test.js
+++ indenttest/test.js
@@ -1,10 +1,10 @@
 for (let i = 0; i < 10; i++) {
-console.log(i);
-let array = [1, 2, 3];
+ console.log(i);
+ let array = [1, 2, 3];
 }
 for (let i = 0; i < 9; i++) {
-console.log(i);
+ console.log(i);
 }
 for (let i = 0; i < 100; i++) {
-console.log(i);
+ console.log(i);
 }
